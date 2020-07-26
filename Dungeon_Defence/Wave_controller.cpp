1c72059bd072f82218180400cd9812749fe2f0c3
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2020-07-26 20:53:40 +0100

    wave system halfway done, levelsystem error hindering development

--- Dungeon_Defence/Wave_controller.cpp
+++ Dungeon_Defence/Wave_controller.cpp
@@ -1,1 +1,52 @@
 // handle wave related stuff
+
+
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <iostream>
+#include "ecm.h"
+#include "system_renderer.h"
+#include "cmp_sprite.h"
+#include "cmp_actor_movement.h"
+#include "Wave_controller.h"
+
+using namespace std;
+using namespace sf;
+
+WaveController::WaveController() {}
+
+
+
+void WaveController::update(double dt) {
+	if (cooldown > 0) { cooldown = cooldown - dt; }
+	if (cooldown <= 0) {
+		//spawn the next wave
+		if (spacing > 0) { spacing = spacing - dt; }
+		if (spacing <= 0) {
+
+			Creeplist.at(creepPointer)->setPosition(ls::getTilePosition(ls::findTiles(ls::END)[1]));
+			Creeplist.at(creepPointer)->setAlive(true);
+			Creeplist.at(creepPointer)->setVisible(true);
+
+			//basic health scale, can be changed later
+			Componentlist.at(creepPointer)->health = 10 * waveNo;
+			Componentlist.at(creepPointer)->reward = 1 * waveNo;
+			//every 5th wave is flying, can be adjusted or changed
+			if (waveNo % 5 == 0) {
+				Componentlist.at(creepPointer)->flying = true;
+			}
+			creepPointer = creepPointer + 1;
+		}
+
+
+		//time between waves, can be adjusted
+		if (creepPointer == 20) {
+
+			cooldown = 30.0f;
+			waveNo = waveNo + 1;
+			creepPointer = 0;
+		}
+	}
+
+}
--- Dungeon_Defence/Wave_controller.h
+++ Dungeon_Defence/Wave_controller.h
@@ -1,1 +1,43 @@
 //handle wave related stuff
+
+
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <iostream>
+#include "ecm.h"
+#include "system_renderer.h"
+#include "cmp_sprite.h"
+#include "scene.h"
+#include "game.h"
+#include "cmp_actor_movement.h"
+#include "levelsystem.h"
+
+
+using namespace std;
+using namespace sf;
+
+class WaveController {
+
+protected:
+	//60 seconds to learn controls, and place towers before 1st wave
+	float cooldown = 60.0f;
+	//time between each creep spawn, can be adjusted to make waves denser or sparser
+	float spacing = 0.5f;
+
+	int creepPointer = 0;
+
+	//what wave we are on, can be used for scaling and special waves
+	int waveNo = 1;
+public:
+	WaveController();
+
+	//i wish i knew a better way to do this right now
+	std::vector<std::shared_ptr<Entity>> Creeplist;
+	std::vector<std::shared_ptr<CreepMovementComponent>> Componentlist;
+
+
+	void update(double dt);
+};
+
+
+
